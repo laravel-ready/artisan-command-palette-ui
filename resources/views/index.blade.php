3f32dfae8f7e356b40b27cde7e8de6dc16275b46
Eyüp <epsilonelittr@gmail.com>
2025-05-08 22:50:22 +0300

    Update default command palette UI favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Artisan Commands</title>
-    <link rel="icon" href="https://fav.farm/👻" />
+    <link rel="icon" href="https://fav.farm/🎮" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script>
